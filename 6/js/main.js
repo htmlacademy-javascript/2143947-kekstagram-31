b9b25f4ba6e16262d35d02c42a16eed60fe75b2b
Keks <keks@htmlacademy.ru>
2024-02-28 21:12:32 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,6 @@
+import * as photos from './get-photos.js';
+
+// eslint-disable-next-line
+console.log(photos.getPhotos(photos.randomIdArray, photos.randomUrlArray, photos.getRandomDescription, photos.getRandomLikes, photos.getComments));
+
+
---
+++ 6/js/util.js
@@ -0,0 +1,29 @@
+export const randomNumberArray = (min, max) => {
+  const array = [];
+  for (let i = min; i <= max; i++) {
+    array.push(i);
+  }
+
+  return array;
+};
+
+export const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  return array;
+};
+
+export const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+export const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
