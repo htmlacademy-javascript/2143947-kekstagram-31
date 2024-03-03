1491341468525ba1f272376a857d375ca276ba89
Keks <keks@htmlacademy.ru>
2024-03-03 15:38:48 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/pictures.js
@@ -0,0 +1,20 @@
+import * as data from './get-photos.js';
+
+
+const otherUsersPicturesList = document.querySelector('.pictures');
+const otherUserPictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+export const otherUserPicture = data.getPhotos(data.randomIdArray, data.randomUrlArray, data.getRandomDescription, data.getRandomLikes, data.getComments);
+
+const otherUserPictureFragment = document.createDocumentFragment();
+
+otherUserPicture.forEach((photo) => {
+  const otherUserPictureElement = otherUserPictureTemplate.cloneNode(true);
+  otherUserPictureElement.querySelector('.picture__img').src = photo.url;
+  otherUserPictureElement.querySelector('.picture__img').alt = photo.description;
+  otherUserPictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
+  otherUserPictureElement.querySelector('.picture__likes').textContent = photo.likes;
+  otherUserPictureFragment.appendChild(otherUserPictureElement);
+});
+
+otherUsersPicturesList.appendChild(otherUserPictureFragment);
