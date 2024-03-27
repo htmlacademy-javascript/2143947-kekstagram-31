d08896d4b19cc56620655638774dc89f2d180838
WStalker <zlokazov.k@gmail.com>
2024-03-27 22:44:08 +0300

    Добавлено меню фильтрации изображений

--- js/pictures.js
+++ js/pictures.js
@@ -2,6 +2,7 @@ import {getData} from './api.js';
 
 export const otherUsersPicturesList = document.querySelector('.pictures');
 const otherUserPictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const imgFiltersContainer = document.querySelector('.img-filters');
 
 const otherUserPictureFragment = document.createDocumentFragment();
 
@@ -16,6 +17,9 @@ getData.forEach((photo) => {
 });
 
 otherUsersPicturesList.appendChild(otherUserPictureFragment);
+console.log(otherUsersPicturesList.children);
+
+imgFiltersContainer.classList.remove('img-filters--inactive');
 
 const temp = getData.reduce((acc, value) => {
   acc.push([value.id.toString(), value]);
@@ -23,3 +27,61 @@ const temp = getData.reduce((acc, value) => {
 }, []);
 
 export const gallery = new Map(temp);
+
+// Переключение кнопок фильтрации изображений и сортировка
+
+const imgFiltersForm = imgFiltersContainer.querySelector('.img-filters__form');
+const imgFiltersButtons = imgFiltersContainer.querySelectorAll('.img-filters__button');
+const RANDOM_PHOTOS_COUNT = 10;
+
+imgFiltersForm.addEventListener('click', (evt) => {
+  for (const button of imgFiltersButtons) {
+    button.classList.remove('img-filters__button--active');
+    evt.target.closest('.img-filters__button').classList.add('img-filters__button--active');
+  }
+
+  if (imgFiltersForm.querySelector('#filter-default').classList.contains('img-filters__button--active')) {
+    console.log('default');
+
+    otherUsersPicturesList.innerHTML = null;
+
+    getData.forEach((photo) => {
+      const otherUserPictureElement = otherUserPictureTemplate.cloneNode(true);
+      otherUserPictureElement.setAttribute('dataId', photo.id);
+      otherUserPictureElement.querySelector('.picture__img').src = photo.url;
+      otherUserPictureElement.querySelector('.picture__img').alt = photo.description;
+      otherUserPictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
+      otherUserPictureElement.querySelector('.picture__likes').textContent = photo.likes;
+      otherUserPictureFragment.appendChild(otherUserPictureElement);
+    });
+
+    otherUsersPicturesList.appendChild(otherUserPictureFragment);
+  }
+
+  if (imgFiltersForm.querySelector('#filter-random').classList.contains('img-filters__button--active')) {
+    console.log('random');
+
+    otherUsersPicturesList.innerHTML = null;
+
+    getData
+      .slice()
+      .slice(0, RANDOM_PHOTOS_COUNT)
+      .forEach((photo) => {
+        const otherUserPictureElement = otherUserPictureTemplate.cloneNode(true);
+        otherUserPictureElement.setAttribute('dataId', photo.id);
+        otherUserPictureElement.querySelector('.picture__img').src = photo.url;
+        otherUserPictureElement.querySelector('.picture__img').alt = photo.description;
+        otherUserPictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
+        otherUserPictureElement.querySelector('.picture__likes').textContent = photo.likes;
+        otherUserPictureFragment.appendChild(otherUserPictureElement);
+      });
+
+    otherUsersPicturesList.appendChild(otherUserPictureFragment);
+  }
+
+  if (imgFiltersForm.querySelector('#filter-discussed').classList.contains('img-filters__button--active')) {
+    console.log('discussed');
+  }
+});
+
+
